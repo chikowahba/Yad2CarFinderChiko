e3013e4d5abed7001c08c4cd20c5b3df8aa86ae3
chikowahba <chikowahba2@gmail.com>
2025-04-03 10:11:15 +0300

    Update Yad2Listing.ts

--- src/types/Yad2Listing.ts
+++ src/types/Yad2Listing.ts
@@ -1,14 +1,15 @@
 export interface Yad2Listing {
-    token: string;
-    price?: number;
-    previousOwner?: { text: string };
-    owner?: { text: string };
-    customer?: { name: string, phone: string };
-    hand?: { text: string };
-    vehicleDates?: { yearOfProduction: number };
-    model?: { textEng: string };
-    manufacturer: { textEng: string };
-    metaData: { coverImage: string };
-    km: number;
-    // We'll keep it minimal as requested - more fields can be added later
-  }
+  token: string;
+  price?: number;
+  previousOwner?: { text: string };
+  owner?: { text: string };
+  customer?: { name: string, phone: string };
+  hand?: { text: string };
+  vehicleDates?: { yearOfProduction: number };
+  model?: { textEng: string };
+  manufacturer: { textEng: string };
+  metaData: { coverImage: string };
+  km: number;
+  address: { city: { text: string } };
+  // We'll keep it minimal as requested - more fields can be added later
+}
